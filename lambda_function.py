21b2776cc1d5d2bce55863308628e951ea4ed4fc
Daniel Fudge <daniel.fudge1977@gmail.com>
2021-02-28 21:12:43 -0500

    Minor formatting on handler.

--- lambda_function.py
+++ lambda_function.py
@@ -1,16 +1,16 @@
 from subprocess import run
 
-def lambda_handler(event, context):
-    
+
+def lambda_handler(event, _):
     print("Event:")
     print(event)
-    
-    cmd = ["./cpp.exe", 
-           event['Records'][0]['s3']['bucket']['name'], 
-           event['Records'][0]['s3']['object']['key'], 
+
+    cmd = ["./cpp.exe",
+           event['Records'][0]['s3']['bucket']['name'],
+           event['Records'][0]['s3']['object']['key'],
            event['Records'][0]['awsRegion']]
-    
+
     print("Command:  " + " ".join(cmd))
     run(cmd)
-    
+
     return 0
